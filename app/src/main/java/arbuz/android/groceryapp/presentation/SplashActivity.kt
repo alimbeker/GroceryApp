3a29a1ed3291eedb24a6adc7ab031d4c8ffdf97a
amiralimbekerov <asuraaka@outlook.com>
2024-05-16 20:14:43 +0500

    add SplashActivity

---
+++ app/src/main/java/arbuz/android/groceryapp/presentation/SplashActivity.kt
@@ -0,0 +1,16 @@
+package arbuz.android.groceryapp.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import arbuz.android.groceryapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+    }
+}
--- app/src/main/java/arbuz/android/groceryapp/presentation/screen/CartFragment.kt
+++ app/src/main/java/arbuz/android/groceryapp/presentation/screen/CartFragment.kt
@@ -1,20 +1,19 @@
-package arbuz.android.groceryapp.presentation
+package arbuz.android.groceryapp.presentation.screen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import arbuz.android.groceryapp.R
 
-class HomeFragment : Fragment() {
+class CartFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
-
 }
