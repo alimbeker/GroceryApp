ff9a78829b92fccfe86e1b4a0aabf0be6c0fcc19
amiralimbekerov <asuraaka@outlook.com>
2024-05-16 21:21:00 +0500

    add splash screen

--- app/src/main/java/arbuz/android/groceryapp/presentation/SplashActivity.kt
+++ app/src/main/java/arbuz/android/groceryapp/presentation/SplashActivity.kt
@@ -1,16 +1,36 @@
 package arbuz.android.groceryapp.presentation
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import arbuz.android.groceryapp.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var timer: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        timer = object : CountDownTimer(2500, 2000) {
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
     }
 }
