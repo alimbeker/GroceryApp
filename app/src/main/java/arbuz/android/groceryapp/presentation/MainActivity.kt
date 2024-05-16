b04b9e82c501e04cbb68a3df7591a5f136b784bf
amiralimbekerov <asuraaka@outlook.com>
2024-05-16 19:23:11 +0500

    adding bottomNavigationView

--- app/src/main/java/arbuz/android/groceryapp/presentation/MainActivity.kt
+++ app/src/main/java/arbuz/android/groceryapp/presentation/MainActivity.kt
@@ -1,14 +1,35 @@
 package arbuz.android.groceryapp.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import arbuz.android.groceryapp.R
+import arbuz.android.groceryapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavView.setOnItemSelectedListener { itemId ->
+            when (itemId) {
+                R.id.homeScreen -> navController.navigate(R.id.homeFragment)
+                R.id.cartScreen -> navController.navigate(R.id.cardFragment)
+            }
+        }
     }
 }
