8b45c52c56677fb27325ad9658a0f8f1fd7e061a
amiralimbekerov <asuraaka@outlook.com>
2024-05-19 15:57:15 +0500

    FragmentCartBinding

--- app/src/main/java/arbuz/android/groceryapp/presentation/screen/CartFragment.kt
+++ app/src/main/java/arbuz/android/groceryapp/presentation/screen/CartFragment.kt
@@ -5,15 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import arbuz.android.groceryapp.R
+import arbuz.android.groceryapp.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
-
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
