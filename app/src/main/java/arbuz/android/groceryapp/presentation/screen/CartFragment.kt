a869983583986b9288e04f01f656e6334eb13aa4
amiralimbekerov <asuraaka@outlook.com>
2024-05-20 13:50:21 +0500

    add CartFragment and setupRecyclerView

--- app/src/main/java/arbuz/android/groceryapp/presentation/screen/CartFragment.kt
+++ app/src/main/java/arbuz/android/groceryapp/presentation/screen/CartFragment.kt
@@ -5,17 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import arbuz.android.groceryapp.data.database.Grocery
 import arbuz.android.groceryapp.databinding.FragmentCartBinding
+import arbuz.android.groceryapp.presentation.adapter.GroceryAdapter
+import arbuz.android.groceryapp.presentation.listener.GroceryItemClickListener
+import arbuz.android.groceryapp.presentation.viewmodel.GroceryViewModel
 
-class CartFragment : Fragment() {
+class CartFragment : Fragment(), GroceryItemClickListener {
+
+    private val viewModel: GroceryViewModel by viewModels()
     private var _binding: FragmentCartBinding? = null
     private val binding get() = _binding!!
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var adapter: GroceryAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentCartBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        observeData()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = GroceryAdapter(this)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun observeData() {
+        viewModel.groceries.observe(viewLifecycleOwner) { groceries ->
+            val filteredGroceries = groceries.filter { it.quantityInCart > 0 }
+            adapter.submitList(filteredGroceries)
+        }
+    }
+
+    override fun onAddToCartClicked(grocery: Grocery) {
+        viewModel.addToCart(grocery)
+    }
+
+    override fun onRemoveFromCartClicked(grocery: Grocery) {
+        viewModel.removeFromCart(grocery)
     }
 
     override fun onDestroyView() {
